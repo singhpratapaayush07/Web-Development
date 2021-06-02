5dfbacd15d67d6a5de986b3495b9c82cba846efe
Aayush Pratap Singh <52466865+singhpratapaayush07@users.noreply.github.com>
2021-06-02 10:12:55 -0700

    Update app.js

--- Basics/ejs/app.js
+++ Basics/ejs/app.js
@@ -2,21 +2,18 @@ const express= require("express");
 
 
 const app = express();
+
+//Setting up View Engine (looks for views folder to get ejs files)
 app.set("view engine","ejs");
+
+//To make use of external files like css, photos etc
 app.use(express.static("public"));
 
-var day="Fucked up";
+var day="Monday";
 
-var val=4;
-if(val==1){
-  day="Weekday";
-}
-else{
-  day="Weekend";
-}
-
+//Home Route
 app.get("/",function(req,res){
-  res.render('list',{ha:day});
+  res.render('list',{ha:day}); //Sending list.ejs (always stored in views Folder)
   //res.send("hello");
 });
 
